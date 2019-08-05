cf00ba250a8c0dd7b64753088bcd15d91701cacf
marz dragon <smartdragon17@gmail.com>
2019-08-05 11:22:16 -0500

    HW1

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+       
+            double nOne, nTwo;
+            
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("<------------------------------------------>~");
+            Console.WriteLine("");
+            
+            //prompt for hits
+            Console.WriteLine("Enter first number: ");
+            nOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            nTwo = Convert.ToInt32(Console.ReadLine());
+
+       
+            Console.WriteLine("The sum is "+ (nOne+nTwo));
+            Console.WriteLine("The product is " + (nOne * nTwo));
+            Console.WriteLine("The difference is " + (nOne - nTwo));
+            Console.WriteLine("The quotient is " + (nOne / nTwo));
+
+            Console.ReadLine();
+        }
+    }
+}
