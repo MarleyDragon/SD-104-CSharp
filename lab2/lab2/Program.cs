359cd0d85c5472e8989aabbec3e5e3d2ab92ee11
marz dragon <smartdragon17@gmail.com>
2019-08-05 11:09:07 -0500

    end save

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -33,6 +33,7 @@ Notes:      This program reads in a baseball player's name,
             dblHits = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the player's number of  at bats: ");
             dblAtBats = Convert.ToInt32(Console.ReadLine());
+
             dblBattingAverage = dblHits / dblAtBats;
             Console.WriteLine(strPlayerName + "'s batting average is " + dblBattingAverage);
             Console.ReadLine();
