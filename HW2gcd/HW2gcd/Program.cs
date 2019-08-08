d46d3be093f3e16be9a3241c9488f4daffcea804
marz dragon <smartdragon17@gmail.com>
2019-08-08 15:14:16 -0500

    end of class

--- HW2gcd/HW2gcd/Program.cs
+++ HW2gcd/HW2gcd/Program.cs
@@ -11,6 +11,8 @@ namespace HW2gcd
         static void Main(string[] args)
         {
             int n1, n2, n3;
+            int nMin=0;
+            int nMax=0;
 
 
             Console.WriteLine("Enter an integer: ");
@@ -18,34 +20,54 @@ namespace HW2gcd
             {
                 Console.WriteLine("Not Valid. Must be a Positive rational number");
             }
+            
 
             Console.WriteLine("Enter another integer or Q to Quit: ");
             while (!int.TryParse(Console.ReadLine(), out n2) || n2 < 0)
             {
                 Console.WriteLine("Not Valid. Must be a Positive rational number");
             }
-
-            n3 = n1 % n2;
-            Console.WriteLine(n3);
-
-            do
+            if (n1 < n2)
             {
-                if (n3 > 0)
-                {
-                    n3 = n1 % n2;
-                    Console.Write("Remainder is " + n3 + ".\r\n");
-                    n1 = n2;
-                    n2 = n3;
-                }
-                else if (n3 == 0)
-                {
-                    Console.WriteLine("Greatest common divisor is " + n2);
-                }
+                
+               nMin=n1;
+               nMax=n2;
+                        
 
             }
-            while (n3 != 0);
+            else
+            {
+                nMax = n1;
+                nMin = n2;
+            }
+
+            
+
+                n3 = nMax % nMin;
+
+                Console.WriteLine("The greatest common divisor between " + nMax + " and " + nMin);
+                do
+                {
+                    if (n3 == 0)
+                    {
+                        Console.WriteLine("is " + nMax + ".");
+                        break;
+                    }
+                    else
+                    {
+                        n3 = nMax % nMin;
+
+                    nMax = nMin;
+                    nMin = n3;
+
+                    }
 
 
+                }
+                while (n3 >= 0);
+
+
+            
 
 
 
