5ae1a02914d056fa98f11d7fd05a5f9a4da87c90
marz dragon <smartdragon17@gmail.com>
2019-09-05 22:19:15 -0500

    bed

--- HandsOn/HandsOn/Program.cs
+++ HandsOn/HandsOn/Program.cs
@@ -10,27 +10,91 @@ namespace HandsOn
     {
         public static void Main(string[] args)
         {
-           public  int[] array1 = { 1, 4, 6, 1, 3, 9, 0, -1, -6, 11 };
-         public   int[] array2 = { -9, -7, -11, -7, -88, -5, -10 };
+            int[] array1 = { 1, 4, 6, 1, 3, 9, 0, -1, -6, 11 };
+            int[] array2 = { -9, -7, -11, -7, -88, -5, -10 };
 
+            int max1 = FindMax(array1);
+            Console.WriteLine("The max number of array 1 is "+max1);
 
+            int max2 = FindMax(array2);
+            Console.WriteLine("The max number of array 2 is " + max2);
+
+            Console.ReadLine();
+
+            Console.WriteLine(" ______________________________________");
+
+            Console.WriteLine("What is the temperature?");
+            float temp = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Press C to convert from Celsius to Fahrenheit;\n" +
+                " or F to convert from Fahrenheit to Celsius:");
+            string tempType = Console.ReadLine();
+            float convertedTemp = ConvertTemp(tempType, temp);
+            Console.ReadLine();
+
+            Console.WriteLine(" ______________________________________");
+
+            float avg = FindAverage(array1);
+            Console.WriteLine(avg);
+            Console.ReadLine();
 
 
         }
-    public static int FindMax(int[] array)
-    {
-        var maxInt = int.MinValue;
-        foreach (var number in array)
+
+
+        static float FindAverage(int[] array)
         {
-            if (number > maxInt)
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
             {
-                maxInt = number;
+
+
+                sum += array[i];
+
             }
+            float avg = sum / array.Length;
+            return avg;
         }
-        return maxInt;
+        static int FindMax(int[] array)
+        {
+            var maxInt = int.MinValue;
+            foreach (var number in array)
+            {
+                if (number > maxInt)
+                {
+                    maxInt = number;
+                }
+            }
+            return maxInt;
+        }
+        static float ConvertTemp(string tempType, float temp)
+        {
+            if (tempType== "C" || tempType== "c")
+            {
+                temp = ((temp * 9) / 5) + 32;
+                Console.WriteLine(temp);
+            }
+            else if (tempType == "F" || tempType == "f")
+            {
+                temp = ((temp - 32) * 5) / 9;
+                Console.WriteLine(temp);
+                
+
+            }
+            return temp;
+        }
+
+
+
     }
+
+
+
+
+
+
+    
 }
 
 
-    }
-}
+
