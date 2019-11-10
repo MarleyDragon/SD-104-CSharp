814718f21097753a06b8bd244d98a12b7ecfc4bf
marz dragon <smartdragon17@gmail.com>
2019-11-09 18:07:27 -0600

    cut

--- CatDragonInn/CatDragonInn/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CatDragonInn.MvcApplication" Language="C#" %>
---
+++ CatDragonInnWebApp/CatDragonInnWebApp/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatDragonInnWebApp.Models
+{
+    public class Customer
+    {
+        [Required]
+        public int CustomerID { get; set; }
+
+        [ForeignKey ("UserId")]
+        public virtual int UserID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set;  }
+
+        public string Image { get; set; }
+
+        public string FavGame { get; set; }
+
+
+
+    }
+}
